dd96ff11da219915fb5c8deb9d4548ce26a0af29
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-12-18 09:34:41 -0600

    Update dependencies

--- src/main/java/net/tylers1066/flags/Flags.java
+++ src/main/java/net/tylers1066/flags/Flags.java
@@ -5,12 +5,12 @@ import net.tylers1066.flags.helper.BlockMaterialFlag;
 import org.bukkit.Material;
 
 public class Flags {
-    public final static SetFlag<Material> ALLOW_BLOCKS = new SetFlag<Material>("allow-blocks", new BlockMaterialFlag(null));
-    public final static SetFlag<Material> DENY_BLOCKS = new SetFlag<Material>("allow-blocks", new BlockMaterialFlag(null));
+    public final static SetFlag<Material> ALLOW_BLOCKS = new SetFlag<>("allow-blocks", new BlockMaterialFlag(null));
+    public final static SetFlag<Material> DENY_BLOCKS = new SetFlag<>("allow-blocks", new BlockMaterialFlag(null));
 
-    public final static SetFlag<Material> ALLOW_BLOCK_PLACE = new SetFlag<Material>("allow-block-place", new BlockMaterialFlag(null));
-    public final static SetFlag<Material> DENY_BLOCK_PLACE = new SetFlag<Material>("deny-block-place", new BlockMaterialFlag(null));
+    public final static SetFlag<Material> ALLOW_BLOCK_PLACE = new SetFlag<>("allow-block-place", new BlockMaterialFlag(null));
+    public final static SetFlag<Material> DENY_BLOCK_PLACE = new SetFlag<>("deny-block-place", new BlockMaterialFlag(null));
 
-    public final static SetFlag<Material> ALLOW_BLOCK_BREAK = new SetFlag<Material>("allow-block-break", new BlockMaterialFlag(null));
-    public final static SetFlag<Material> DENY_BLOCK_BREAK = new SetFlag<Material>("deny-block-break", new BlockMaterialFlag(null));
+    public final static SetFlag<Material> ALLOW_BLOCK_BREAK = new SetFlag<>("allow-block-break", new BlockMaterialFlag(null));
+    public final static SetFlag<Material> DENY_BLOCK_BREAK = new SetFlag<>("deny-block-break", new BlockMaterialFlag(null));
 }
